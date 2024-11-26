658617405b900628c423bd59a76465dd78e4036c
Saboor Hassan <156575127+HsnSaboor@users.noreply.github.com>
2024-11-26 10:48:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import patoolib
 import os
 from pathlib import Path
+from urllib.parse import unquote
 
 # Function to download file from URL
 def download_file(url, path):
@@ -38,12 +39,12 @@ def main():
         if url:
             temp_dir = Path.cwd() / "temp_files"
             temp_dir.mkdir(parents=True, exist_ok=True)
-            file_name = Path(url).name
+            file_name = unquote(Path(url).name)
             file_path = temp_dir / file_name
             download_file(url, file_path)
             if file_path.exists() and file_path.is_file():
                 if file_name.endswith(('.zip', '.rar', '.7z', '.tar.gz')):
-                    extract_dir = temp_dir / file_name.stem
+                    extract_dir = temp_dir / Path(file_name).stem
                     extract_dir.mkdir(parents=True, exist_ok=True)
                     extract_archive(file_path, extract_dir)
                     # Further processing of extracted files
